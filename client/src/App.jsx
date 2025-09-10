81dd73499e7d2f74da6f2ad0897bb81acae2c010
vishxlkr <vishxlkr@gmail.com>
2025-09-10 23:03:08 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
 import About from "./pages/About";
 import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
 
 const App = () => {
    return (
       <div className="mx-4 sm:mx-[10%]">
+         <Navbar />
          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/doctors" element={<Doctors />} />
