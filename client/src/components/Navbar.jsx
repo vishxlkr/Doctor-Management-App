fe1ebdea222e6711be4372354b46805c5cd852ef
vishxlkr <vishxlkr@gmail.com>
2025-10-05 02:09:16 +0530

    update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+   const navigate = useNavigate();
+
+   const [showMenu, setShowMenu] = useState(false);
+   const [token, setToken] = useState(true);
+
    return (
-      <div>
-         <img src={assets.logo} alt="" />
-         <ul>
-            <NavLink>
-               <li>HOME</li>
+      <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+         <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
+         <ul className="idden md:flex items-start gap-5 font-medium">
+            <NavLink to="/">
+               <li className="py-1">HOME</li>
+               <hr className="border-none outline-none h-0.5 bg-primary hidden w-3/5 m-auto" />
+            </NavLink>
+            <NavLink to="/doctors">
+               <li className="py-1">ALL DOCTORS</li>
+               <hr className="border-none outline-none h-0.5 bg-primary hidden w-3/5 m-auto" />
+            </NavLink>
+            <NavLink to="/about">
+               <li className="py-1">ABOUT</li>
+               <hr className="border-none outline-none h-0.5 bg-primary hidden w-3/5 m-auto" />
+            </NavLink>
+            <NavLink to="/contact">
+               <li className="py-1">CONTACT</li>
+               <hr className="border-none outline-none h-0.5 bg-primary hidden w-3/5 m-auto" />
             </NavLink>
          </ul>
+         <div className="flex items-center gap-4">
+            {token ? (
+               <div></div>
+            ) : (
+               <button
+                  onClick={() => navigate("/login")}
+                  className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block"
+               >
+                  Create account
+               </button>
+            )}
+         </div>
       </div>
    );
 };
