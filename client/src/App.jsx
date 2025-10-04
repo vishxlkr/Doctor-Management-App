cf45efdd9f15c0bad2d0a5bb2a50fb86975c3391
vishxlkr <vishxlkr@gmail.com>
2025-10-05 01:07:36 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import About from "./pages/About";
+import Login from "./pages/Login";
 
 const App = () => {
    return (
       <div className="mx-4 sm:mx-[10%]">
          <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/doctors" element={<Doctors />} />
+            <Route path="/doctors/:speciality" element={<Doctors />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/my-profile" element={<MyProfile />} />
+            <Route path="/my-appointments" element={<MyAppointments />} />
+            <Route path="/appointment/:docId" element={<appoin />} />
          </Routes>
       </div>
    );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-   return <div>home</div>;
+   return <div>homepage</div>;
 };
 
 export default Home;
