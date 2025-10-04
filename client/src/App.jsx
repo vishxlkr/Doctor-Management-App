4e47986d3526c9c68a95a77c90b4dd400fbcf8a0
vishxlkr <vishxlkr@gmail.com>
2025-10-05 00:51:31 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-   return <div>hello world</div>;
+   return (
+      <div className="mx-4 sm:mx-[10%]">
+         <Routes>
+            <Route path="/" element={<Home />} />
+         </Routes>
+      </div>
+   );
 };
 
 export default App;
