603a535c5a597536ab04138d0b0920b96060886e
vishxlkr <vishxlkr@gmail.com>
2025-10-05 20:11:56 +0530

    update

--- client/src/components/TopDoctors.jsx
+++ client/src/components/TopDoctors.jsx
@@ -8,16 +8,19 @@ const TopDoctors = () => {
          <p className="sm:w-1/3 text-center text-sm">
             Simply browse through our extensive list of trusted doctors.
          </p>
-         <div className="w-fu11 grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+         <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
             {doctors.slice(0, 10).map((item, index) => (
-               <div className=" border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 ">
-                  <img className="bg-blue-50 " src={item.image} alt="" />
+               <div className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-500">
+                  <img className="bg-blue-50" src={item.image} alt="" />
+
                   <div className="p-4">
                      <div className="flex items-center gap-2 text-sm text-center text-green-500 ">
-                        <p className="w-2 h-2 bg-green-500 rounded-full"></p>{" "}
+                        <p className="w-2 h-2 bg-green-500 rounded-full"></p>
                         <p>Available</p>
                      </div>
+
                      <p>{item.name}</p>
+
                      <p>{item.speciality}</p>
                   </div>
                </div>
