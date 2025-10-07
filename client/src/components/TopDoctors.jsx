8dcb875f08773605db47541008e594107adcf606
vishxlkr <vishxlkr@gmail.com>
2025-10-08 00:58:04 +0530

    update

--- client/src/components/TopDoctors.jsx
+++ client/src/components/TopDoctors.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { doctors } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const TopDoctors = () => {
+   const navigate = useNavigate();
+
    return (
       <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
          <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
@@ -10,7 +13,10 @@ const TopDoctors = () => {
          </p>
          <div className="w-full grid grid-cols-[var(--template-grid-cols-auto)] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
             {doctors.slice(0, 10).map((item, index) => (
-               <div className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-500">
+               <div
+                  onClick={() => navigate(`/appointment/${item._id}`)}
+                  className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-500"
+               >
                   <img className="bg-blue-50" src={item.image} alt="" />
 
                   <div className="p-4">
@@ -19,16 +25,18 @@ const TopDoctors = () => {
                         <p>Available</p>
                      </div>
 
-                     <p className="text-gray-900 text-lg font-medium ">
+                     <p className="text-gray-900 text-lg font-medium">
                         {item.name}
                      </p>
 
-                     <p>{item.speciality}</p>
+                     <p className="text-gray-600 text-sm ">{item.speciality}</p>
                   </div>
                </div>
             ))}
          </div>
-         <button>More</button>
+         <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10 cursor-pointer hover:bg-blue-100 transition">
+            More
+         </button>
       </div>
    );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
    <BrowserRouter>
-      <App />
+      <AppContextProvider>
+         <App />
+      </AppContextProvider>
    </BrowserRouter>
 );
