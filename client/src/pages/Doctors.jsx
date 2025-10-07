488ee28387f8133456207060156af5f7394d65df
vishxlkr <vishxlkr@gmail.com>
2025-10-08 02:47:13 +0530

    update

--- client/src/pages/Doctors.jsx
+++ client/src/pages/Doctors.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Doctors = () => {
+   const { speciality } = useParams();
+
+   console.log(speciality);
+
    return <div>doctors</div>;
 };
 
