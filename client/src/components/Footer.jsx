0a396619a4290771b938f5357ddef3c897315dd0
vishxlkr <vishxlkr@gmail.com>
2025-10-08 02:23:53 +0530

    update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { assets } from "../assets/assets";
 
 const Footer = () => {
    return (
-      <div>
-         <div>
+      <div className="md:mx-10">
+         <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 -mt-40 text-sm">
             {/* ---------left section------- */}
             <div>
-               <img src={assets.logo} alt="" />
-               <p>
+               <img className="mb-5 w-40" src={assets.logo} alt="" />
+               <p className="w-full md:w-2/3 text-gray-600 leading-6">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Velit est temporibus et dolore praesentium odit magni deleniti
                   consequuntur sed inventore.
@@ -16,8 +16,8 @@ const Footer = () => {
             </div>
             {/* --------center section---------- */}
             <div>
-               <p>Company</p>
-               <ul>
+               <p className="text-xl font-medium mb-5">COMPANY</p>
+               <ul className="flex flex-col gap-2 text-gray-600">
                   <li>Home</li>
                   <li>About Us</li>
                   <li>Contact Us</li>
