9a42c6c9c7c1960f34ce020b1ba08a61db3d57e7
vishxlkr <vishxlkr@gmail.com>
2025-10-17 01:40:51 +0530

    update

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -2,10 +2,35 @@
 
 const addDoctor = async (req, res) => {
    try {
+      const {
+         name,
+         email,
+         password,
+         speciality,
+         degree,
+         experience,
+         about,
+         fees,
+         address,
+      } = req.body;
+
+      const imageFile = req.file;
+
+      console.log(
+         {
+            name,
+            email,
+            password,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            address,
+         },
+         imageFile
+      );
    } catch (error) {}
 };
 
-
-git quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quick
-git quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quick
-git quickgit quickgit quickgit quickgit quick
+export { addDoctor };
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 
 // app config
 const app = express();
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoint
+app.use("/api/admin", adminRouter);
+// localhost:4000/api/admin/add-doctor
 
 app.get("/", (req, res) => {
    res.send("api working");
