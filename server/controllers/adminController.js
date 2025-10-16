0dfc00e9c3ae3ee7b0d09467afb22b58fc395ead
vishxlkr <vishxlkr@gmail.com>
2025-10-16 10:40:37 +0530

    update

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -8,4 +8,4 @@ const addDoctor = async (req, res) => {
 
 git quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quick
 git quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quick
-git quickgit quick
+git quickgit quickgit quickgit quick
