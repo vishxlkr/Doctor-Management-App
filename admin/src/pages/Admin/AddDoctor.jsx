727598108419a1b70f6fff341da401fba42b64e4
vishxlkr <vishxlkr@gmail.com>
2025-10-17 13:20:01 +0530

    update

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import { assets } from "../../assets/assets";
 
 const AddDoctor = () => {
-   return <div>addDoctor</div>;
+   return (
+      <form>
+         <p> Add Doctor</p>
+         <div>
+            <div>
+               <label htmlFor="doc-img">
+                  <img src={assets.upload_area} alt="" />
+               </label>
+               <input type="file" id="doc-img" hidden />
+               <p>
+                  Upload doctor <br /> picture
+               </p>
+            </div>
+
+            <div>
+               <div>
+                  <div>
+                     <p>Your name</p>
+                     <input type="text" placeholder="Name" required />
+                  </div>
+                  <div>
+                     <p>Doctor Email</p>
+                     <input type="email" placeholder="Email" required />
+                  </div>
+                  <div>
+                     <p>Doctor Password</p>
+                     <input type="password" placeholder="Password" required />
+                  </div>
+                  <div>
+                     <p> Experience</p>
+                     <select name="" id="">
+                        <option value="1 Year">1 Year</option>
+                        <option value="2 Year">2 Year</option>
+                        <option value="1 Year">1 Year</option>
+                        <option value="1 Year">1 Year</option>
+                        <option value="1 Year">1 Year</option>
+                        <option value="1 Year">1 Year</option>
+                        <option value="1 Year">1 Year</option>
+                        <option value="1 Year">1 Year</option>
+                        <option value="1 Year">1 Year</option>
+                        <option value="1 Year">1 Year</option>
+                        <option value="1 Year">1 Year</option>
+                     </select>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </form>
+   );
 };
 
 export default AddDoctor;
