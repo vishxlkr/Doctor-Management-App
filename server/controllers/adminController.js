a6699678a69af164010fdc765d9d9189a5b0f8ba
vishxlkr <vishxlkr@gmail.com>
2025-10-17 17:52:50 +0530

    update

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -108,4 +108,16 @@ const loginAdmin = async (req, res) => {
    }
 };
 
-export { addDoctor, loginAdmin };
+// api to get all doctors list for admin panel
+
+const allDoctors = async (req, res) => {
+   try {
+      const doctors = await doctorModel.find({}).select("-password");
+      res.json({ success: true, doctors });
+   } catch (error) {
+      console.log(error);
+      res.json({ success: false, message: error.message });
+   }
+};
+
+export { addDoctor, loginAdmin, allDoctors };
