e6a5213abf6d573d509e4dcb7e996fc8cc3f3de5
vishxlkr <vishxlkr@gmail.com>
2025-10-17 20:40:38 +0530

    update

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -9,11 +9,7 @@ const AppContextProvider = (props) => {
    const currencySymbol = "$";
    const backendUrl = import.meta.env.VITE_BACKEND_URL;
    const [doctors, setDoctors] = useState([]);
-
-   const value = {
-      doctors,
-      currencySymbol,
-   };
+   const [token, setToken] = useState("");
 
    const getDoctorsData = async () => {
       try {
@@ -27,6 +23,14 @@ const AppContextProvider = (props) => {
          console.log(error);
          toast.error(error.message);
       }
+   };
+
+   const value = {
+      doctors,
+      currencySymbol,
+      token,
+      setToken,
+      backendUrl,
    };
 
    useEffect(() => {
