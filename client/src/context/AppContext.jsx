0a64d8ed0d81de9a55713cfa4e289e2099f981ec
vishxlkr <vishxlkr@gmail.com>
2025-10-18 21:36:41 +0530

    update

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -12,7 +12,7 @@ const AppContextProvider = (props) => {
    const [token, setToken] = useState(
       localStorage.getItem("token") ? localStorage.getItem("token") : false
    );
-   const [userDate, setUserData] = useState(false);
+   const [userData, setUserData] = useState(false);
 
    const getDoctorsData = async () => {
       try {
@@ -28,17 +28,44 @@ const AppContextProvider = (props) => {
       }
    };
 
+   const loadUserProfileData = async () => {
+      try {
+         const { data } = await axios.get(
+            backendUrl + "/api/user/get-profile",
+            { headers: { token } }
+         );
+         if (data.success) {
+            setUserData(data.userData);
+         } else {
+            toast.error(data.message);
+         }
+      } catch (error) {
+         console.log(error);
+         toast.error(error.message);
+      }
+   };
+
    const value = {
       doctors,
       currencySymbol,
       token,
       setToken,
       backendUrl,
+      userData,
+      setUserData,
+      loadUserProfileData,
    };
 
    useEffect(() => {
       getDoctorsData();
    }, []);
+   useEffect(() => {
+      if (token) {
+         loadUserProfileData();
+      } else {
+         setUserData(false);
+      }
+   }, [token]);
 
    return (
       <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
