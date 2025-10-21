64552c9af8d4724ce98dc15deeee5d7c77e30c17
vishxlkr <vishxlkr@gmail.com>
2025-10-21 12:57:00 +0530

    update

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -23,4 +23,4 @@ adminRouter.post("/cancel-appointment", authAdmin, appointmentCancel);
 export default adminRouter;
 
 git quickgit quickgit quickgit quickgit quickgit quickgit quick
-git quickgit quick
+git quickgit quickgit quickv
