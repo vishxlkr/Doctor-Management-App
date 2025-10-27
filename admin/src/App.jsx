88f5c4bfaa53ca2e833ca854e692a97acca965a1
vishxlkr <vishxlkr@gmail.com>
2025-10-27 19:15:34 +0530

    update

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -56,3 +56,4 @@ const App = () => {
 export default App;
 
 git quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quickgit quick
+git quick
